5208d57411b98ac0e99a242b84056c2b1591aca0
metisssi <simcheraandrij@gmail.com>
2025-09-07 02:07:17 +0200

    Initial commit with chat functionality

---
+++ public/main.js
@@ -0,0 +1,111 @@
+const socket = io()
+
+const clientTotal = document.getElementById('clients-total')
+
+const nameInput = document.getElementById('name-input')
+const messageContainer = document.getElementById('message-container')
+const messageForm = document.getElementById('message-form')
+const messageInput = document.getElementById('message-input')
+
+
+messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    sendMessage()
+})
+socket.on('clients-total', (data) => {
+    clientTotal.innerHTML = `
+        Total Clients: ${data}
+    `
+})
+
+
+function sendMessage() {
+
+    if (messageInput.value === '') return
+
+
+    // console.log(messageInput.value)
+
+    const data = {
+        name: nameInput.value,
+        message: messageInput.value,
+        dateTime: new Date()
+    }
+
+    socket.emit('message', data)
+    addMessageToUi(true, data)
+    messageInput.value = ''
+}
+
+
+socket.on('chat-message', (data) => {
+
+    addMessageToUi(false, data)
+})
+
+
+function addMessageToUi(isOwnMessage, data) {
+
+    clearFeedback()
+    
+    const element = `
+        <li class="${isOwnMessage ? "message-right" : "message-left"}">
+                <p class="message">
+                    ${data.message}
+                    <span>${data.name} ${moment(data.dateTime).fromNow()}</span>
+                </p>
+            </li>
+    `
+
+
+    messageContainer.innerHTML += element
+    scrollToBottom()
+}
+
+
+
+function scrollToBottom() {
+    messageContainer.scrollTo(0, messageContainer.scrollHeight)
+}
+
+
+
+messageInput.addEventListener('focus', (e) => {
+    socket.emit('feedback', {
+        feedback: `${nameInput.value} is typing a message`
+    })
+})
+
+messageInput.addEventListener('keypress', (e) => {
+    socket.emit('feedback', {
+        feedback: `${nameInput.value} is typing a message`
+    })
+})
+
+messageInput.addEventListener('blur', (e) => {
+    socket.emit('feedback', {
+        feedback: ``
+    })
+})
+
+
+socket.on('feedback', (data) => {
+    clearFeedback()
+    const element = `
+     <li class="message-feedback">
+                <p class="feedback" id="feedback">
+                    ${data.feedback}
+                </p>
+            </li>
+    `
+
+
+    messageContainer.innerHTML += element
+})
+
+
+function clearFeedback() {
+    document.querySelectorAll('li.message-feedback').forEach(element => {
+        element.parentNode.removeChild(element )
+    })
+}
